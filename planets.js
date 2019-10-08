600931ac1ad5d02d6b30d1453d7eef3ab1cee80f
Christopher Paskewich <c.paskewich@centre.edu>
2019-10-08 10:59:32 -0400

    lab work from class

--- planets.js
+++ planets.js
@@ -1,40 +1,51 @@
+var planetPromise=d3.json("planets.json")
+    planetPromise.then(
+    function(data)
+        {
+            A1(data)
+            A2(data)
+            B3(data)
+        } )
 
-
-
-
-
-
-/*
-var makePlanet = function(name,img,distance,radius,density,moons)
+//A1
+var Names=["one","two","three"]
+var A1 = function(data)
 {
-    return {name,img,distance,radius,density,moons};
+    d3.select("#A1").append("p").text(Names);
 }
 
+//A2
+var A2=function(data)
+    {
+    var pDiv=d3.select("#A2")        
+    .selectAll("div")
+    .data(Names)
+    .enter()
+    .append("div")
+    .text(function(d)
+         {
+        return d 
+    })
+        }
+   
+//A3
+var planetPromise=d3.json("planets.json")
+    planetPromise.then(
+    function(data)
+        {
+            console.log(planetPromise)
+        })
 
-var planets = [
-makePlanet("Mercury","img/mercury.jpg",
-.38,.38,5.43,0),
-makePlanet("Venus","img/venus.jpg",
-.72,.94,5.24,0),
-makePlanet("Earth","img/earth.jpg",
-1,1,5.52,1),
-makePlanet("Mars","img/mars.jpg",
-1.52,.53,3.94,2),
-makePlanet("Jupiter","img/jupiter.jpg",
-5.2,11.21,1.33,79),
-makePlanet("Saturn","img/saturn.jpg",
-9.53,9.45,.7,62),
-makePlanet("Uranus","img/uranus.jpg",
-19.19,4.0,1.3,27),
-makePlanet("Neptune","img/neptune.jpg",
-30,3.88,1.76,14),
-];
-
-console.log(JSON.stringify(planets,null,2))
-
-*/
-
-
-
-
-                      
+//A4
+var B3=function(data)
+    {
+        var pDiv=d3.select("#B3")
+        .selectAll("img")
+        .data("planets.json")
+        .enter()
+        .append("img")
+        .attr("src", function(d)
+             {
+            return d
+        })
+    }
